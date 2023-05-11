84fd6c67911796cd517867b2a9c1c75524d44309
stropee <stropee@campus-eni.fr>
2023-05-11 16:58:54 +0200

    TP 5

--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\WishRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,15 +10,40 @@ use Symfony\Component\Routing\Annotation\Route;
 class WishController extends AbstractController
 {
     #[Route('/', name: 'list')]
-    public function list(): Response
+    public function list(WishRepository $wishRepository): Response
     {
-        return $this->render('wish/list.html.twig');
+        //récupération des wish publiés triés par date
+        $wishes = $wishRepository->findBy(["isPublished" => true], ["dateCreated" => "DESC"]);
+
+        return $this->render('wish/list.html.twig', [
+            'wishes' => $wishes
+        ]);
     }
 
     #[Route('/{id}', name: 'show', requirements: ["id" => "\d+"])]
-    public function show(int $id): Response
+    public function show(int $id, WishRepository $wishRepository): Response
     {
-        dump($id);
-        return $this->render('wish/show.html.twig');
+        $wish = $wishRepository->find($id);
+
+        if(!$wish){
+            throw $this->createNotFoundException("Oops ! Wish not found !");
+        }
+
+        return $this->render('wish/show.html.twig', [
+            'wish' => $wish
+        ]);
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
