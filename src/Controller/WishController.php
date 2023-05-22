d000bcc0118f1cc7693b3a0516b137b81708fc99
stropee <stropee@campus-eni.fr>
2023-05-22 14:46:22 +0200

    TP 6

--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Wish;
+use App\Form\WishType;
 use App\Repository\WishRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 #[Route('/wish', name: 'wish_')]
@@ -31,6 +34,30 @@ class WishController extends AbstractController
 
         return $this->render('wish/show.html.twig', [
             'wish' => $wish
+        ]);
+    }
+    #[Route('/add', name: 'add')]
+    public function add(Request $request, WishRepository $wishRepository) : Response {
+
+        $wish = new Wish();
+        $wishForm = $this->createForm(WishType::class, $wish);
+
+        $wishForm->handleRequest($request);
+
+        if($wishForm->isSubmitted() && $wishForm->isValid()){
+
+//            $wish->setDateCreated(new \DateTime());
+//            $wish->setIsPublished(true);
+
+            $wishRepository->save($wish, true);
+
+            $this->addFlash('success', "Wish added !");
+
+            return $this->redirectToRoute('wish_show', ['id' => $wish->getId()]);
+        }
+
+        return $this->render('wish/add.html.twig', [
+            'wishForm' => $wishForm->createView()
         ]);
     }
 
